c8f4e4afe9851e1f15ad079880b86bfdbe1514b9
Megan Walford <mwalford22@gmail.com>
2024-01-24 17:59:38 +0000

    Waiting for API allowance to restore

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,7 +2,6 @@
 //(user) has many object methods, 
 function userInformationHTML(user) {
     return `
-        //displays the users public display name
         <h2>${user.name}
             <span class="small-name">
                 (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
@@ -16,6 +15,27 @@ function userInformationHTML(user) {
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -34,11 +54,15 @@ function fetchGitHubInformation(event) {
         </div>`);
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+
     ).then(
-        function (response) {
-            var userData = response;
+        function (firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0]
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function (errorResponse) {
             if (errorResponse.status === 404) {
