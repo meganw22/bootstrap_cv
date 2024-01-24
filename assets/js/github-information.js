c84d624cde8a3372d86404caa5be83a310199af7
Megan Walford <mwalford22@gmail.com>
2024-01-24 17:41:54 +0000

    Render user information from GitHub

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,23 @@
+//function to display user information on the screen
+//(user) has many object methods, 
+function userInformationHTML(user) {
+    return `
+        //displays the users public display name
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -13,21 +33,23 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-        $.when(
-            $.getJSON(`https://api.github.com/users/${username}`)
-        ).then(
-            function(response) {
-                var userData = response;
-                $("#gh-user-data").html(userInformationHTML(userData));
-            },
-            function(errorResponse) {
-                if (errorResponse.status === 404) {
-                    $("#gh-user-data").html(
-                        `<h2>No info found for user ${username}</h2>`);
-                } else {
-                    console.log(errorResponse);
-                    $("#gh-user-data").html(
-                        `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-                }
-            });
-    }
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function (response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function (errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+}
+
+
